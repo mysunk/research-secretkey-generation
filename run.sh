ae9a82f10d9b1c19b62f92d6c9cbf6f5233bb060
myungsunkim <rlaaudtjs@gist.ac.kr>
2020-10-14 14:35:58 +0900

    clear git cache

--- run.sh
+++ run.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+# change dataset from (1,3,5) to 1-11
+python dnn-ga.py --result_save_dir=1014/2-1 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --N_CHILDREN=10 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=4 --PROB_MUTATION=0.05
+python dnn-ga.py --result_save_dir=1014/2-2 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --N_CHILDREN=10 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=4 --PROB_MUTATION=0.1
+python dnn-ga.py --result_save_dir=1014/2-3 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=20 --N_CHILDREN=10 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=4 --PROB_MUTATION=0.05
+python dnn-ga.py --result_save_dir=1014/2-4 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=20 --N_CHILDREN=10 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=4 --PROB_MUTATION=0.1
+python dnn-ga.py --result_save_dir=1014/2-5 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --N_CHILDREN=20 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=4 --PROB_MUTATION=0.05
+python dnn-ga.py --result_save_dir=1014/2-6 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --N_CHILDREN=20 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=4 --PROB_MUTATION=0.1
+python dnn-ga.py --result_save_dir=1014/2-7 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=20 --N_CHILDREN=20 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=4 --PROB_MUTATION=0.05
+python dnn-ga.py --result_save_dir=1014/2-8 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=20 --N_CHILDREN=20 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=4 --PROB_MUTATION=0.1
+
+
+# =============================================================================================================================
+# Previous simulations
+# =============================================================================================================================
+
 # python dnn-ga.py --result_save_dir=1010/1 --EPOCHS=100 --N_POPULATION=100 --N_BEST=10 --h1=30 --h2=30 --h3=30
 # python dnn-ga.py --result_save_dir=1010/2 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=128 --h2=128 --h3=128
 
@@ -14,6 +29,7 @@
 # python dnn-ga.py --result_save_dir=1011/7 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=512 --h2=512 --h3=512 --early_stopping=300
 
 # == score function option changed ===
+# change dataset from (1,3,5) to (1-9)
 # python dnn-ga.py --result_save_dir=1012/1 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=128 --h2=128 --h3=128 --early_stopping=50 --score_type=0
 # python dnn-ga.py --result_save_dir=1012/2 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=128 --h2=128 --h3=128 --early_stopping=50 --score_type=1
 # python dnn-ga.py --result_save_dir=1012/3 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=128 --h2=128 --h3=128 --early_stopping=50 --score_type=2
@@ -28,6 +44,37 @@
 # python dnn-ga.py --result_save_dir=1013/7 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=50 --score_type=7
 # python dnn-ga.py --result_save_dir=1013/8 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=50 --score_type=8
 # python dnn-ga.py --result_save_dir=1013/9 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=50 --score_type=9
-python dnn-ga.py --result_save_dir=1013/10 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=50 --score_type=10
-python dnn-ga.py --result_save_dir=1013/11 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=50 --score_type=11
-python dnn-ga.py --result_save_dir=1013/12 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=50 --score_type=12
+# python dnn-ga.py --result_save_dir=1013/10 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=50 --score_type=10
+# python dnn-ga.py --result_save_dir=1013/11 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=50 --score_type=11
+# python dnn-ga.py --result_save_dir=1013/12 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=50 --score_type=12
+# python dnn-ga.py --result_save_dir=1013/13 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=128 --h2=128 --h3=128 --early_stopping=50 --score_type=13
+# python dnn-ga.py --result_save_dir=1013/14 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=128 --h2=128 --h3=128 --early_stopping=100 --score_type=10
+# python dnn-ga.py --result_save_dir=1013/15 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=256 --h2=256 --h3=256 --early_stopping=50 --score_type=13
+# python dnn-ga.py --result_save_dir=1013/16 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=256 --h2=256 --h3=256 --early_stopping=100 --score_type=10
+# python dnn-ga.py --result_save_dir=1013/17 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=512 --h2=512 --h3=512 --early_stopping=50 --score_type=13
+# python dnn-ga.py --result_save_dir=1013/18 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=512 --h2=512 --h3=512 --early_stopping=100 --score_type=10
+
+# 10-13-2
+# ===  Change score function option from 1 ===
+# python dnn-ga.py --result_save_dir=1013_2/1 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=1
+# python dnn-ga.py --result_save_dir=1013_2/2 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=2
+# python dnn-ga.py --result_save_dir=1013_2/3 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=3
+# python dnn-ga.py --result_save_dir=1013_2/4 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=4
+# python dnn-ga.py --result_save_dir=1013_2/5 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=5
+# python dnn-ga.py --result_save_dir=1013_2/6 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=6
+# python dnn-ga.py --result_save_dir=1013_2/7 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=7
+# python dnn-ga.py --result_save_dir=1013_2/8 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=8
+# python dnn-ga.py --result_save_dir=1013_2/4_2 --EPOCHS=1000 --N_POPULATION=50 --N_BEST=5 --N_CHILDREN=5 --h1=128 --h2=128 --h3=128 --early_stopping=100 --score_type=4
+
+# 10-14
+# change dataset from (1-9) to (1,3,5)
+# python dnn-ga.py --result_save_dir=1014/1-1 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --N_CHILDREN=20 --h1=32 --h2=32 --h3=32 --early_stopping=100 --score_type=4
+# python dnn-ga.py --result_save_dir=1014/1-2 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --N_CHILDREN=20 --h1=64 --h2=64 --h3=64 --early_stopping=100 --score_type=4
+# python dnn-ga.py --result_save_dir=1014/1-3 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --N_CHILDREN=20 --h1=16 --h2=16 --h3=16 --early_stopping=100 --score_type=4 --random_seed=0
+# python dnn-ga.py --result_save_dir=1014/1-4 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --N_CHILDREN=20 --h1=32 --h2=32 --h3=32 --early_stopping=100 --score_type=4 --random_seed=0
+#python dnn-ga.py --result_save_dir=1014/1-5 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --N_CHILDREN=20 --h1=32 --h2=32 --h3=32 --early_stopping=100 --score_type=4 --random_seed=0 --PROB_MUTATION=0.05
+#python dnn-ga.py --result_save_dir=1014/1-6 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --N_CHILDREN=10 --h1=32 --h2=32 --h3=32 --early_stopping=100 --score_type=4 --random_seed=1
+#python dnn-ga.py --result_save_dir=1014/1-7 --EPOCHS=1000 --N_POPULATION=50 --N_BEST=10 --N_CHILDREN=20 --h1=32 --h2=32 --h3=32 --early_stopping=100 --score_type=4 --random_seed=2
+#python dnn-ga.py --result_save_dir=1014/1-8 --EPOCHS=1000 --N_POPULATION=150 --N_BEST=10 --N_CHILDREN=20 --h1=32 --h2=32 --h3=32 --early_stopping=100 --score_type=4 --random_seed=3
+#python dnn-ga.py --result_save_dir=1014/1-9 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --N_CHILDREN=20 --h1=32 --h2=32 --h3=32 --early_stopping=100 --score_type=4 --random_seed=0 --mutation_std=2
+#python dnn-ga.py --result_save_dir=1014/1-10 --EPOCHS=1000 --N_POPULATION=100 --N_BEST=10 --N_CHILDREN=20 --h1=32 --h2=32 --h3=32 --early_stopping=100 --score_type=4 --random_seed=0 --PROB_MUTATION=0.1
---
+++ test.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import numpy as np
+from genome import *
+import matplotlib.pyplot as plt
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--result_save_dir", default='tmp', type=str)
+args = parser.parse_args()
+result_save_dir = 'results/' + args.result_save_dir
+
+def minmax_norm(CSI_data1):
+    max_v1 = np.max(CSI_data1)
+    min_v1 = np.min(CSI_data1)
+    CSI_data1 = (CSI_data1 - min_v1) / (max_v1 - min_v1)
+    return (CSI_data1)
+
+if __name__ == '__main__':
+    # load dataset
+    CSI_data_ref = pd.read_csv('data_in_use/gain_1.csv', header=None)
+    CSI_data_ref = CSI_data_ref.values.T
+    CSI_data_ref = minmax_norm(CSI_data_ref)
+
+    import pickle
+    with open(result_save_dir + '/best_genomes.pkl','rb') as f:
+        best_genomes = pickle.load(f)
+
+    # load all
+    CSI_datas = []
+    num_samples = 40
+    for i in range(1, num_samples + 1):
+        CSI_data = pd.read_csv('data_in_use/gain_' + str(i) + '.csv', header=None)
+        # Transpose
+        CSI_data = CSI_data.values.T
+        # Min-max normalization
+        CSI_data = minmax_norm(CSI_data)
+        CSI_datas.append(CSI_data)
+
+    #%% check hamming distance
+    # predict
+    codewords_ref = best_genomes[0].predict(CSI_data_ref)
+    # plot
+    dists = np.zeros((1000, len(CSI_datas)), dtype=int)
+    dists_same_loc = np.zeros((1000, len(CSI_datas)), dtype=int)
+    # random sampling
+    max_i = CSI_data_ref.shape[0]
+    codewords = []
+    for j in range(len(CSI_datas)):
+        codewords_2 = best_genomes[0].predict(CSI_datas[j])
+        codewords.append(codewords_2)
+        for i in range(1000):
+            # for same sample
+            dists[i, j] = hamming_distance(codewords_ref[np.random.randint(0, max_i, 1)[0],:], codewords_2[np.random.randint(0, max_i, 1)[0],:])
+            dists_same_loc[i,j] = hamming_distance(codewords_2[np.random.randint(0, max_i, 1)[0],:], codewords_2[np.random.randint(0, max_i, 1)[0],:])
+    codewords = np.concatenate(codewords, axis=0)
+
+    locs = np.arange(0, 0.06*num_samples, 0.06)
+
+    plt.figure(figsize=(10,5))
+    plt.boxplot(x=dists)
+    plt.xticks(range(num_samples)[::4], locs[::4])
+    plt.ylabel('Hamming distance')
+    # plt.xlabel('Sample location #')
+    plt.xlabel('d / lambda')
+    plt.tight_layout()
+    plt.savefig(result_save_dir + '/hamming_dist.png')
+    # plt.show()
+
+    plt.figure(figsize=(10,5))
+    plt.boxplot(x=dists_same_loc)
+    plt.xticks(range(num_samples)[::4], locs[::4])
+    plt.ylabel('Hamming distance')
+    # plt.xlabel('Sample location #')
+    plt.xlabel('d / lambda')
+    plt.tight_layout()
+    plt.savefig(result_save_dir + '/hamming_dist_same_loc.png')
+    # plt.show()
+
+    #%% scatter plot
+    # get distance
+    from genome import score
+    CSI_data_all = np.concatenate(CSI_datas, axis=0)
+    codeword_ref = best_genomes[0].predict(np.mean(CSI_data_ref, axis=0))
+    dist_X, dist_C, _ = score(CSI_data_all, codewords, codeword_ref, 1)
+
+    plt.figure()
+    plt.scatter(dist_X, dist_C)
+    plt.xlabel('Euclidean distance')
+    plt.ylabel('Hamming distance')
+    plt.savefig(result_save_dir + '/hamming_dist_scatter.png')
+    #plt.show()
+
+    # save codewords
+    codewords = pd.DataFrame(data = np.concatenate(codewords, axis=0))
+    codewords.to_csv(result_save_dir+'/codewords.csv', index=False)
