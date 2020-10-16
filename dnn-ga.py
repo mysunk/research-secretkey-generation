6aac95d5af8a860e04e12913462f3cb57e55591a
myungsunkim <rlaaudtjs@gist.ac.kr>
2020-10-16 20:44:26 +0900

    Add baseline model

--- dnn-ga.py
+++ dnn-ga.py
@@ -11,7 +11,8 @@ from copy import deepcopy
 from genome import Genome, genome_score, hamming_distance
 import time
 import argparse
-import numpy as np
+from AE.load_dataset import *
+
 warnings.filterwarnings(action='ignore')
 
 # User input
@@ -35,35 +36,34 @@ parser.add_argument("--random_seed", default=76, type=int)
 parser.add_argument("--score_type", default=1, type=int)
 args = parser.parse_args()
 
-
-#%% Hyperparameters
-CPU_CORE = multiprocessing.cpu_count()          # 멀티프로세싱 CPU 사용 수
-print(f'Use CPU_CORE: {CPU_CORE}')
-REVERSE = False                                 # 배열 순서 (False: ascending order, True: descending order) == maximize
-score_ini = 10e+10                              # 초기 점수
-np.random.seed(args.random_seed)                # random seed
-batch_size = args.batch_size                    # batch size
-input_length = args.input_length                # subcarrier 수
-output_length = args.output_length              # codeword length
-h1 = args.h1                                    # 히든레이어1 노드 수
-h2 = args.h2                                    # 히든레이어2 노드 수
-h3 = args.h3                                    # 히든레이어3 노드 수
-EPOCHS = args.EPOCHS                            # 반복 횟수
-early_stopping = args.early_stopping            # saturation시 early stopping
-crossover_fraction = args.crossover_fraction    # crossover 비율
-N_POPULATION = args.N_POPULATION                # 세대당 생성수
-N_BEST = args.N_BEST                            # 베스트 수
-N_CHILDREN = args.N_CHILDREN                    # 자손 유전자 수
-PROB_MUTATION = args.PROB_MUTATION              # 돌연변이
-mutation_std = args.mutation_std                # 돌연변이시 standard deviation
+# %% Hyperparameters
+CPU_CORE = multiprocessing.cpu_count()  # 멀티프로세싱 CPU 사용 수
+REVERSE = False  # 배열 순서 (False: ascending order, True: descending order) == maximize
+score_ini = 10e+10  # 초기 점수
+np.random.seed(args.random_seed)  # random seed
+batch_size = args.batch_size  # batch size
+input_length = args.input_length  # subcarrier 수
+output_length = args.output_length  # codeword length
+h1 = args.h1  # 히든레이어1 노드 수
+h2 = args.h2  # 히든레이어2 노드 수
+h3 = args.h3  # 히든레이어3 노드 수
+EPOCHS = args.EPOCHS  # 반복 횟수
+early_stopping = args.early_stopping  # saturation시 early stopping
+crossover_fraction = args.crossover_fraction  # crossover 비율
+N_POPULATION = args.N_POPULATION  # 세대당 생성수
+N_BEST = args.N_BEST  # 베스트 수
+N_CHILDREN = args.N_CHILDREN  # 자손 유전자 수
+PROB_MUTATION = args.PROB_MUTATION  # 돌연변이
+mutation_std = args.mutation_std  # 돌연변이시 standard deviation
 
 # plot save dir
-result_save_dir = 'results/'+args.result_save_dir
+result_save_dir = 'results/' + args.result_save_dir
 import os
+
 if not os.path.exists(result_save_dir):
     os.makedirs(result_save_dir)
 
-#%% Initial guess
+# %% Initial guess
 genomes = []
 for _ in range(N_POPULATION):
     genome = Genome(score_ini, input_length, output_length, h1, h2, h3, batch_size, init_weight=None)
@@ -73,10 +73,11 @@ try:
         genomes[i] = best_genomes[i]
 except:
     best_genomes = []
-    for _ in range(N_BEST): # genome의 개수 조정
+    for _ in range(N_BEST):  # genome의 개수 조정
         genome = Genome(score_ini, input_length, output_length, h1, h2, h3, batch_size, init_weight=None)
         best_genomes.append(genome)
 print('==Process 1 Done==')
+
 
 def crossover(process_1, process_2, new_process, weight, crossover_fraction):
     # crossover
@@ -89,67 +90,74 @@ def crossover(process_1, process_2, new_process, weight, crossover_fraction):
         new_weight[j, cut == 0] = getattr(process_2, weight)[j, cut == 0]
     return new_weight
 
+
 def mutation(new_process, mean, stddev, weight):
     # mutation event
     w_e = getattr(new_process, weight).shape[0]
     h_e = getattr(new_process, weight).shape[1]
     if np.random.uniform(0, 1) < PROB_MUTATION:
-        new_weight = new_process.__dict__[weight] * np.random.normal(mean, stddev, size=(w_e, h_e)) * np.random.randint(0, 2, (w_e, h_e))
+        new_weight = new_process.__dict__[weight] * np.random.normal(mean, stddev, size=(w_e, h_e)) * np.random.randint(
+            0, 2, (w_e, h_e))
     else:
         new_weight = new_process.__dict__[weight]
     return new_weight
 
-#%% 모델 학습
-n_gen = 1 
+
+# %% 모델 학습
+n_gen = 1
 score_history = []
 high_score_history = []
 mean_score_history = []
 t_start = time.time()
 while n_gen <= EPOCHS:
-    genomes = np.array(genomes)    
-    while len(genomes)%CPU_CORE != 0:
-        genomes = np.append(genomes, Genome(score_ini, input_length, output_length, h1, h2, h3, batch_size,init_weight=None))
-    genomes = genomes.reshape((len(genomes)//CPU_CORE, CPU_CORE))
-    
+    genomes = np.array(genomes)
+    while len(genomes) % CPU_CORE != 0:
+        genomes = np.append(genomes,
+                            Genome(score_ini, input_length, output_length, h1, h2, h3, batch_size, init_weight=None))
+    genomes = genomes.reshape((len(genomes) // CPU_CORE, CPU_CORE))
+
     for idx, _genomes in enumerate(genomes):
         if __name__ == '__main__':
             from _functools import partial
-            partial_func = partial(genome_score, score_type = args.score_type)
+
+            partial_func = partial(genome_score, score_type=args.score_type)
             # hyper parameters
             pool = multiprocessing.Pool(processes=CPU_CORE)
             genomes[idx] = pool.map(partial_func, _genomes)
             pool.close()
             pool.join()
-    genomes = list(genomes.reshape(genomes.shape[0]*genomes.shape[1]))    
-    
-     # score에 따라 정렬
+    genomes = list(genomes.reshape(genomes.shape[0] * genomes.shape[1]))
+
+    # score에 따라 정렬
     genomes.sort(key=lambda x: x.score, reverse=REVERSE)
-    
+
     # 평균 점수
-    s = 0 
+    s = 0
     for i in range(N_BEST):
         s += genomes[i].score
     s /= N_BEST
-    
+
     # Best Score
-    bs = genomes[0].score 
-    
+    bs = genomes[0].score
+
     # Best Model 추가
     if best_genomes is not None:
         genomes.extend(best_genomes)
-        
+
     # score에 따라 정렬
     genomes.sort(key=lambda x: x.score, reverse=REVERSE)
-    
+
     score_history.append([n_gen, genomes[0].score])
     high_score_history.append([n_gen, bs])
     mean_score_history.append([n_gen, s])
-    
+
     # 결과 출력
     t = time.time()
-    epoch_duration = (t - t_start)/n_gen
-    print('EPOCH #{}\tHistory Best Score: {:.4f}\tBest Score: {:.4f}\tMean Score: {:.4f}\t Avg time for 1 epoch: {:.3f} seconds'.format(n_gen, genomes[0].score, bs, s, epoch_duration))
-    
+    epoch_duration = (t - t_start) / n_gen
+    print(
+        'EPOCH #{}\tHistory Best Score: {:.4f}\tBest Score: {:.4f}\tMean Score: {:.2f}\t Avg time for 1 epoch: {:.2f} seconds'.format(
+            n_gen, genomes[0].score, bs, s, epoch_duration))
+
     # 모델 업데이트
     best_genomes = deepcopy(genomes[:N_BEST])
 
@@ -158,8 +166,9 @@ while n_gen <= EPOCHS:
         new_genome = deepcopy(best_genomes[0])
         genome_1 = np.random.choice(best_genomes)
         genome_2 = np.random.choice(best_genomes)
-        for weight in ['w1','w2','w3','w4','b1','b2','b3','b4']:
-            new_genome.network.__dict__[weight] = crossover(genome_1.network, genome_2.network, new_genome.network, weight, crossover_fraction)
+        for weight in ['w1', 'w2', 'w3', 'w4', 'b1', 'b2', 'b3', 'b4']:
+            new_genome.network.__dict__[weight] = crossover(genome_1.network, genome_2.network, new_genome.network,
+                                                            weight, crossover_fraction)
         genomes.append(new_genome)
 
     # 모델 초기화
@@ -170,16 +179,20 @@ while n_gen <= EPOCHS:
             mean = 0
             stddev = mutation_std
             # Mutation (PROB_MUTATION의 확률로)
-            for weight in ['w1','w2','w3','w4','b1','b2','b3','b4']:
+            for weight in ['w1', 'w2', 'w3', 'w4', 'b1', 'b2', 'b3', 'b4']:
                 new_genome.network.__dict__[weight] = mutation(new_genome.network, mean, stddev, weight)
             genomes.append(new_genome)
-            
+
     if REVERSE:
         if bs < score_ini:
-            genomes[len(genomes)//2:] = [Genome(score_ini, input_length, output_length, h1, h2, h3, batch_size,init_weight=None) for _ in range(N_POPULATION//2)]
+            genomes[len(genomes) // 2:] = [
+                Genome(score_ini, input_length, output_length, h1, h2, h3, batch_size, init_weight=None) for _ in
+                range(N_POPULATION // 2)]
     else:
         if bs > score_ini:
-            genomes[len(genomes)//2:] = [Genome(score_ini, input_length, output_length, h1, h2, h3, batch_size,init_weight=None) for _ in range(N_POPULATION//2)]
+            genomes[len(genomes) // 2:] = [
+                Genome(score_ini, input_length, output_length, h1, h2, h3, batch_size, init_weight=None) for _ in
+                range(N_POPULATION // 2)]
 
     # early stopping
     if n_gen > early_stopping:
@@ -213,9 +226,13 @@ plt.ylim(bottom=0)
 plt.xlabel('Epochs')
 plt.ylabel('Score')
 plt.tight_layout()
-plt.text(0,0.1,'start score: {:.4f}+///end score: {:.4f}'.format(high_score_history[:,1][0],high_score_history[:,1][-1]))
 plt.savefig(result_save_dir+'/ga_train.png')
 # plt.show()
+f = open(result_save_dir + '/test_result.txt','w+')
+f.write('start score: {:.4f}\n'.format(high_score_history[:,1][0]))
+f.write('end score: {:.4f}'.format(high_score_history[:,1][-1]))
+f.close()
+
 
 # save the best genome
 import pickle
--- genome.py
+++ genome.py
@@ -1,11 +1,5 @@
 from scipy.special import expit
-import pandas as pd
-import numpy as np
-def minmax_norm(CSI_data1):
-    max_v1 = np.max(CSI_data1)
-    min_v1 = np.min(CSI_data1)
-    CSI_data1 = (CSI_data1 - min_v1) / (max_v1 - min_v1)
-    return (CSI_data1)
+from AE.load_dataset import *
 
 # load GNT
 CSI_data_ref = pd.read_csv('data_in_use/gain_1.csv', header=None)
@@ -15,7 +9,7 @@ X_GNT = np.mean(CSI_data_ref, axis=0)
 
 # load all
 CSI_datas = []
-for i in range(1,9,2):
+for i in range(1,11,1):
     CSI_data = pd.read_csv('data_in_use/gain_' + str(i) + '.csv', header=None)
     # Transpose
     CSI_data = CSI_data.values.T
@@ -25,7 +19,6 @@ for i in range(1,9,2):
 CSI_datas = np.concatenate(CSI_datas, axis=0)
 
 class network():
-
 
     def __init__(self, input_len, output_len, hidden_layer1, hidden_layer2, hidden_layer3, init_weight):
         # initialize weight
@@ -37,6 +30,7 @@ class network():
         self.b2 = np.random.randn(1, hidden_layer2) * np.sqrt(1 / hidden_layer2)
         self.b3 = np.random.randn(1, hidden_layer3) * np.sqrt(1 / hidden_layer3)
         self.b4 = np.random.randn(1, output_len) * np.sqrt(1 / output_len)
+
 
     def sigmoid(self, x):
         return expit(x)
@@ -59,7 +53,7 @@ class network():
         net = (net - np.mean(net)) / np.std(net)  # batch normalization
         score = self.sigmoid(net)
         # 이부분 수정 필요
-        score[score > 0.5] = 1
+        score[score>0.5] = 1
         score = score.astype(int)
         return score
 
@@ -69,11 +63,11 @@ class Genome():
     def __init__(self, score_ini, input_length, output_length, h1, h2, h3, batch_size, init_weight=None):
         self.network = network(input_length, output_length, h1, h2, h3, init_weight=None)
 
-        self.dist_X = None
-        self.dist_C = None
-
         # 평가 점수 초기화
         self.score = score_ini
+
+        self.X_dist = None
+        self.C_dist = None
 
     def predict(self, data):
         codewords = self.network.forward(data)
@@ -90,87 +84,23 @@ def hamming_distance(x, y):
             result += 1
     return result
 
-def score(X, output, C_GNT, score_type):
+def score(CSI_data, CSI_data_ref, output, codeword_ref, score_type):
     # calculate distance of X and C
-    distance_X = np.linalg.norm(X_GNT - X, axis=1)
+    distance_X = np.linalg.norm(CSI_data_ref - CSI_data, axis=1)
     distances_C = np.zeros((output.shape[0]))
     for i in range(output.shape[0]):
-        distances_C[i] = hamming_distance(np.ravel(C_GNT), output[i])
+        distances_C[i] = hamming_distance(np.ravel(codeword_ref), output[i])
 
-    # vaying score function
-    # ratio_factor = np.zeros(distance_X.shape)
-    # ratio_factor[distance_X <= 1] = 0
-    # ratio_factor[distance_X > 1] = 0.04
+    # varying ratio factor
     ratio_factor = np.zeros(distance_X.shape)
     ratio_factor[distance_X <= 1] = 0
-    ratio_factor[distance_X > 1] = np.random.randn((distance_X > 1).sum()) * np.sqrt(0.1) + 40
-    score_val = np.mean(np.abs(distance_X * ratio_factor - distances_C))
-    # score_val = np.mean(np.abs(ratio_factor - distances_C/output.shape[0]))
-    return distance_X, distances_C, score_val
+    # ratio_factor[distance_X > 1] = 0.01 * score_type
+    ratio_factor[distance_X > 1] = 2.5 * score_type
+    return distance_X, distances_C, np.sqrt(np.mean((distance_X / CSI_data.shape[1] * ratio_factor - distances_C / output.shape[1]) ** 2))
 
 
 def genome_score(genome, score_type):
     codeword_ref = genome.predict(X_GNT)
     codewords = genome.predict(CSI_datas)
-    genome.dist_X, genome.dist_C, genome.score = score(CSI_datas, codewords, codeword_ref, score_type)
+    genome.X_dist, genome.C_dist, genome.score = score(CSI_datas,X_GNT, codewords, codeword_ref, score_type)
     return genome
-
-
-""" 1013
- # varying ratio factor
-    if score_type == 1:
-        ratio_factor = 1
-    elif score_type == 2:
-        ratio_factor = 30
-    elif score_type == 3:
-        ratio_factor = np.exp(distance_X)
-    elif score_type == 4:
-        ratio_factor = np.exp(distance_X)*3
-    elif score_type == 5:
-        ratio_factor = np.zeros(distance_X.shape)
-        ratio_factor[distance_X <= 1] = 0
-        ratio_factor[distance_X > 1] = np.random.randn((distance_X > 1).sum()) * np.sqrt(0.1) + 17.5
-    elif score_type == 6:
-        ratio_factor = np.nanmax(distances_C / distance_X)
-    elif score_type == 7:
-        ratio_factor = np.median(codeword_ref) / np.median(X_GNT)
-    elif score_type == 8:
-        ratio_factor = np.zeros(distance_X.shape)
-        ratio_factor[distance_X <= 1] = 0
-        ratio_factor[distance_X > 1] = np.random.randn((distance_X > 1).sum()) * np.sqrt(0.1) + 30
-    elif score_type == 9:
-        ratio_factor = np.exp(distance_X)*5
-    elif score_type == 10:
-        ratio_factor = np.zeros(distance_X.shape)
-        ratio_factor[distance_X <= 1] = 0
-        ratio_factor[distance_X > 1] = 242 / distance_X[distance_X > 1]
-    elif score_type == 11:
-        ratio_factor = np.zeros(distance_X.shape)
-        ratio_factor[distance_X <= 1] = 0
-        ratio_factor[distance_X > 1] = np.random.randn((distance_X > 1).sum()) * np.sqrt(0.1) + 40
-    elif score_type == 12:
-        ratio_factor = np.zeros(distance_X.shape)
-        ratio_factor[distance_X <= 1] = 0
-        ratio_factor[distance_X > 1] = np.random.randn((distance_X > 1).sum()) * np.sqrt(0.1) + 50
-    elif score_type == 13:
-        ratio_factor = np.zeros(distance_X.shape)
-        ratio_factor[distance_X <= 1] = 0
-        ratio_factor[distance_X > 1] = np.random.randn((distance_X > 1).sum()) * np.sqrt(0.1) + 60
-    else:
-        raise NotImplementedError('Not implemented type of score function')
-"""
-
-"""
-    if score_type == 1:
-        ratio_factor = np.zeros(distance_X.shape)
-        ratio_factor[distance_X <= 1] = 0
-        ratio_factor[distance_X > 1] = 0.5 / distance_X[distance_X > 1]
-    elif score_type == 2:
-        ratio_factor = np.zeros(distance_X.shape)
-        ratio_factor[distance_X <= 1] = 0
-        ratio_factor[distance_X > 1] = np.exp(distance_X[distance_X > 1])
-    elif score_type == 3:
-        ratio_factor = np.zeros(distance_X.shape)
-        ratio_factor[distance_X <= 1] = 0
-        ratio_factor[distance_X > 1] =  0.3 / distance_X[distance_X>1]
-"""
